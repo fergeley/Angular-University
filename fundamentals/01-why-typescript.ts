a46daa3540d1e0fa1ba3f19b8152b79e8c623506
GGIsaiah <isaiah@glyphgridsoft.com>
2026-01-22 18:08:51 +0800

    Initial Commit

---
+++ fundamentals/01-why-typescript.ts
@@ -0,0 +1,8 @@
+const courseName = "TypeScript Bootcamp";
+
+// printCourseName("100"); 
+printCourseName(courseName);
+
+function printCourseName(name: string) {
+    console.log("The course name is " + name.toUpperCase());
+}
---
+++ fundamentals/dist/01-why-typescript.js
@@ -0,0 +1,7 @@
+"use strict";
+const courseName = "TypeScript Bootcamp";
+// printCourseName("100"); 
+printCourseName(courseName);
+function printCourseName(name) {
+    console.log("The course name is " + name.toUpperCase());
+}
