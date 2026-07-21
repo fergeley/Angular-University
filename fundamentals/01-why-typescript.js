1e65a0c2884e5fafb7dc232e18807b6a01465b70
isaiah <fergeley@gmail.com>
2026-07-21 16:23:27 +0800

    Angular University: Angular fundamentals course and TypeScript examples

---
+++ fundamentals/01-why-typescript.js
@@ -0,0 +1,7 @@
+var courseName = "TypeScript Bootcamp";
+
+// printCourseName("100"); 
+printCourseName(courseName);
+function printCourseName(name) {
+    console.log("The course name is " + name.toUpperCase());
+}
